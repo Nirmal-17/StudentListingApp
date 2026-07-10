6d2e70f9d3fb3e8f0533a2b007a692db8beb99b7
Nirmal Dahal <nirmaldahal19@gmail.com>
2026-07-10 14:42:44 +0545

    Add files via upload

---
+++ Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace cruddotnet9.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Address { get; set; } = string.Empty;
+
+        public int Age { get; set; }
+       }
+}
