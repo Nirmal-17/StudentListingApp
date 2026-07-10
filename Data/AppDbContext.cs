138803acb46733571c0e767d963e89cb4b8becd5
Nirmal Dahal <nirmaldahal19@gmail.com>
2026-07-10 14:43:20 +0545

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using cruddotnet9.Models;
+
+namespace cruddotnet9.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; } = null!;
+    }
+}
