c12567862bbae376d9a44ea90da2041d60bd9b28
Nirmal Dahal <nirmaldahal19@gmail.com>
2026-07-10 14:41:27 +0545

    Add files via upload

---
+++ Views/Student/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cruddotnet9.Views.Student
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
